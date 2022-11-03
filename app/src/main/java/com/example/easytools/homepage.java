212dd7e5cf06718210459acc6ada39cf9d2ef6bb
Anirudh M <82684635+BoildSoap@users.noreply.github.com>
2022-11-03 10:30:42 -0500

    added logout button functionality

--- app/src/main/java/com/example/easytools/homepage.java
+++ app/src/main/java/com/example/easytools/homepage.java
@@ -1,15 +1,24 @@
 package com.example.easytools;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class homepage extends AppCompatActivity {
-
+    public void logOutClicked(View view) {
+        SignUpLoginActivity.firebaseHelper.logOutUser();
+        Intent intent = new Intent(homepage.this, SignUpLoginActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
     }
+
 }
 
 
